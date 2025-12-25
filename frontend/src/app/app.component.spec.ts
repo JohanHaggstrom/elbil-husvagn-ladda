f2423a4fbe7512685fda382d7c5611a7a63353d3
Johan Häggström <johan.haggstrom@gmail.com>
2025-12-25 21:46:08 +0100

    Adds Docker publishing and deployment setup
    Sets up a GitHub Actions workflow to build and push Docker images for
    the frontend and backend on release.
    
    Also includes Dockerfiles for both frontend and backend, along with a
    docker-compose.yml for local development and deployment.
    Finally, configures Nginx for the frontend.

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have the 'charge-ev-with-caravan' title`, () => {
+    it(`should have the 'elbil-husvagn-ladda' title`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('charge-ev-with-caravan');
+        expect(app.title).toEqual('elbil-husvagn-ladda');
     });
 
     it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
         expect(compiled.querySelector('h1')?.textContent).toContain(
-            'Hello, charge-ev-with-caravan'
+            'Hello, elbil-husvagn-ladda'
         );
     });
 });
