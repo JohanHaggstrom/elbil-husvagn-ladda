60ba880322670bf70532aedadaf205ce762e5585
Johan Häggström <johan.haggstrom@gmail.com>
2026-04-16 20:03:22 +0200

    Bumped frontend version
    Also added reference to where the backend version can be set.

--- frontend/src/app/version.ts
+++ frontend/src/app/version.ts
@@ -1,1 +1,4 @@
-export const FRONTEND_VERSION = '0.66.01';
+// Backend version is set in the backend project file
+// ...\backend\ElbilHusvagnLadda.WebApi\ElbilHusvagnLadda.WebApi.csproj
+
+export const FRONTEND_VERSION = '0.67.01';
